d30781ea206228387bcaa41909a69037809675c7
Kavinkumar L <Kavinkumar.L@odessainc.com>
2025-04-12 17:12:36 +0530

    Add project files.

---
+++ Dev.API/Repositary/DevDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Dev.API.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Dev.API.Repositary
+{
+    public class DevDbContext: DbContext
+    {
+        public DevDbContext(DbContextOptions dbContextOptions) :base(dbContextOptions)
+        {
+            
+        }
+
+        public DbSet<Difficulty> Difficulties { get; set; }
+        public DbSet<Region> Regions { get; set; }
+        public DbSet<Walk> Walks { get; set; }
+    }
+}
